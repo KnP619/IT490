5151a63a8fd74a82fb49b5db8937dcc4a050c8e8
KnP619 <knp33@njit.edu>
2016-10-10 20:44:07 -0400

    Tested with rabbitmq

--- login_processor.php
+++ login_processor.php
@@ -3,35 +3,70 @@ require_once __DIR__ . '/vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
+
 $connection = new AMQPStreamConnection('localhost', 5672, 'admin', 'knp33');
 $channel = $connection->channel();
 $channel->queue_declare('rpc_queue', false, false, false, false);
 
-function dbConnect(){
-	global $dbh; 
-	$dbh = mysqli_connect("localhost", "root", "knp33", "UserInfo") or die(mysqli_error());
-	}
+
 function login($JsonCred) {
-	dbConnect(); 
+    $dbconn = new  mysqli("localhost","root","knp33");
+		if ($dbconn->connect_error) {
+		die("<html>Connection failed:".$dbconn->connect_error."</html>");
+		}
+	
+	echo  $JsonCred;
+	$db = mysqli_select_db($dbconn, "UserInfo");
 	$json=json_decode($JsonCred,true);
-	$username= $json["Username"];
-	$password=$json["Password"];
+	$UserName= $json["Username"];
+	$Password=$json["Password"];
+	//$submit=$json["submit"];
 
-	$s="select * from RegistrationPage  WHERE UserName='$UserName' or Email='$Email' "; 
-	$t= mysqli_query($dbh,$s);
-	$count = mysqli_num_rows($t);
-	if($count > 0) {
-		header("location:success.html");
+
+	if( $UserName !== null && $Password !== null){ 
+		$query = mysqli_query($dbconn, "select * from Login where UserName='$UserName' AND Password='$Password'");
+		$rows = mysqli_num_rows($query);
+		echo "<br>Rows: $rows <br>";
+		if ($rows == 1) //db returned true
+		{ 
+			//$_SESSION["user_token"] = $UserName;
+			//$user_check= $_SESSION["user_token"];				
+			//echo PHP_EOL. "UserCheck $: " . $_SESSION["user_token"];
+			//header("Location: profile.php");
+			return true;
+		}
+	else {
+		echo "<html> UserName or Password is invalid </html>";
+		return false;
 	}
-	else
-	{
-		echo "Invalid user";
-		exit;
-	}	
 }
+					  
+
+					 
+	
+
+	/*if($_SESSION["user_token"] !== null){
+		$user_check=$_SESSION["user_token"];
+	//echo "User check: $user_check";
+	}
+	else{
+	// SQL Query To Fetch Complete Information Of User
+	$query=mysqli_query($dbconn, "select UserName from Login where UserName='$user_check'");
+	$row = mysqli_fetch_assoc($query);
+	echo"<br>" .PHP_EOL ."Row: $row";
+	$login_session =$row['UserName'];
+	echo "$login_session";
+
+	if(!isset($login_session)){
+	mysqli_close($dbconn); // Closing dbconn
+	header('Location: loginform.php'); // Redirecting To Home Page
+//echo "<br>$error";
+	}
+}*/
 
 
 
+/**
 if (is_array($JsonCred) || is_object($JsonCred))
 {
     foreach ($JsonCred as $value)
@@ -41,6 +76,7 @@ if (is_array($JsonCred) || is_object($JsonCred))
     }   
 	return($JsonCred);
 }
+**/}
 echo " [x] Awaiting RPC requests\n";
 $callback = function($req) {
 	$n = ($req->body);
@@ -48,10 +84,10 @@ $callback = function($req) {
 
 //	echo " [.] recieved(",(string) fib($n), ")\n";
 	$msg = new AMQPMessage(
-		(string)fib($n),
+		(string)login($n),
 		array('correlation_id' => $req->get('correlation_id'))
 		);	
-
+ 	//echo "(string)$n";
 	$req->delivery_info['channel']->basic_publish(
 		$msg, '', $req->get('reply_to'));
 	$req->delivery_info['channel']->basic_ack(
