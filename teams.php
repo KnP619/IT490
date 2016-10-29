de3351ac4ada2170f5c4c41efa0ded159abc5cd2
KnP619 <knp33@njit.edu>
2016-10-28 23:09:52 -0400

    List of teams with <option> tag

---
+++ teams.php
@@ -0,0 +1,113 @@
+<?php
+echo "<optionv>FC Bayern MÃ¼nchen </option>";
+echo "<option> Red Bull Leipzig </option>";
+echo "<option>Hertha BSC</option>";
+echo "<option>TSG 1899 Hoffenheim</option>";
+echo "<option>1. FC KÃ¶ln</option>";
+echo "<option>Borussia Dortmund</option>";
+echo "<option>Eintracht Frankfurt</option>";
+echo "<option>SC Freiburg</option>";
+echo "<option>1. FSV Mainz 05</option>";
+echo "<option>Bor. MÃ¶nchengladbach</option>";
+echo "<option>Bayer Leverkusen</option>";
+echo "<option>FC Augsburg</option>";
+echo "<option>SV Darmstadt 98</option>";
+echo "<option>FC Schalke 04</option>";
+echo "<option>Werder Bremen</option>";
+echo "<option>VfL Wolfsburg</option>";
+echo "<option>FC Ingolstadt 04</option>";
+echo "<option>Hamburger SV</option>";
+
+echo "<option>OGC Nice</option>";
+echo "<option>AS Monaco FC</option>";
+echo "<option>Paris Saint-Germain</option>";
+echo "<option>Toulouse FC</option>";
+echo "<option>EA Guingamp</option>";
+echo "<option>Stade Rennais FC</option>";
+echo "<option>AS Saint-Ã‰tienne</option>";
+echo "<option>FC Girondins de Bordeaux</option>";
+echo "<option>Angers SCO</option>";
+echo "<option>Olympique Lyonnais</option>";
+echo "<option>Olympique de Marseille</option>";
+echo "<option>FC Metz</option>";
+echo "<option>Dijon FCO</option>";
+echo "<option>FC Nantes</option>";
+echo "<option>SC Bastia</option>";
+echo "<option>OSC Lille</option>";
+echo "<option>Montpellier HÃ©rault SC</option>";
+echo "<option>SM Caen</option>";
+echo "<option>AS Nancy</option>";
+echo "<option>FC Lorient</option>";
+
+echo "<option>Manchester City FC</option>";
+echo "<option>Arsenal FC</option>";
+echo "<option>Liverpool FC</option>";
+echo "<option>Chelsea FC</option>";
+echo "<option>Tottenham Hotspur FC</option>";
+echo "<option>Everton FC</option>";
+echo "<option>Manchester United FC</option>";
+echo "<option>Southampton FC</option>";
+echo "<option>Watford FC</option>";
+echo "<option>AFC Bournemouth</option>";
+echo "<option>Crystal Palace FC</option>";
+echo "<option>Leicester City FC</option>";
+echo "<option>West Bromwich Albion FC</option>";
+echo "<option>Burnley FC</option>";
+echo "<option>West Ham United FC</option>";
+echo "<option>Stoke City FC</option>";
+echo "<option>Middlesbrough FC</option>";
+echo "<option>Hull City FC</option>";
+echo "<option>Swansea City FC</option>";
+echo "<option>Sunderland AFC</option>";
+
+echo "<option>Real Madrid CF </option>";
+echo "<option>Sevilla FC</option>";
+echo "<option>FC Barcelona</option>";
+echo "<option>Villarreal CF</option>";
+echo "<option>Club AtlÃ©tico de Madrid</option>";
+echo "<option>Athletic Club</option>";
+echo "<option>Real Sociedad de FÃºtbol</option>";
+echo "<option>RC Celta de Vigo</option>";
+echo "<option>UD Las Palmas</option>";
+echo "<option>MÃ¡laga CF</option>";
+echo "<option>SD Eibar</option>";
+echo "<option>Real Betis</option>";
+echo "<option>Deportivo AlavÃ©s</option>";
+echo "<option>CD Leganes</option>";
+echo "<option>Valencia CF</option>";
+echo "<option>RCD Espanyol</option>";
+echo "<option>RC Deportivo La Coruna</option>";
+echo "<option>Sporting GijÃ³n</option>";
+echo "<option>CA Osasuna</option>";
+echo "<option>Granada CF</option>";
+
+echo "<option>Juventus Turin</option>";
+echo "<option>AS Roma</option>";
+echo "<option>SSC Napoli</option>";
+echo "<option>AC Milan</option>";
+echo "<option>SS Lazio</option>";
+echo "<option>Atalanta BC</option>";
+echo "<option>Torino FC</option>";
+echo "<option>Genoa CFC</option>";
+echo "<option>AC Chievo Verona</option>";
+echo "<option>FC Internazionale Milano</option>";
+echo "<option>ACF Fiorentina</option>";
+echo "<option>Udinese Calcio</option>";
+echo "<option>Bologna FC</option>";
+echo "<option>US Sassuolo Calcio</option>";
+echo "<option>Cagliari Calcio</option>";
+echo "<option>UC Sampdoria</option>";
+echo "<option>Pescara Calcio</option>";
+echo "<option>Empoli FC</option>";
+echo "<option>US CittÃ¡ di Palermo</option>";
+echo "<option>FC Crotone</option>";
+?>
+
+
+
+
+
+
+
+
+
