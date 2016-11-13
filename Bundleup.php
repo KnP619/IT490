691f33c9caee0f2c60164e4a0781a85254d9fdef
KnP619 <knp33@njit.edu>
2016-11-13 18:47:17 -0500

    now can pass arguments

--- Bundleup.php
+++ Bundleup.php
@@ -1,22 +1,28 @@
 <?php
-$file = fopen("bundle.ini", "r") or die("Unable to open file!");
-$archive = new PharData('Backend.tar');
-//$archive->addFile('text.txt');
-//$archive->compress(Phar::GZ);
-$parse = parse_ini_file("bundle.ini",true);
-mkdir('update',0777,true);
-foreach($parse as $arr){
-    foreach($arr as $source){
-		$key = array_search($source, $arr);
-		copy($source, __DIR__.'/update/'.$key);
-		//echo 'Key: ' .$key . PHP_EOL;
-		//echo $val . PHP_EOL;
-		
-	}
-}
-copy("bundle.ini",__DIR__.'/update/bundle.ini');
-$archive->buildFromDirectory(dirname(__FILE__) . '/update');
-//$archive->compress(Phar::GZ);
-rmdir('update');
 
+if($argc != 3 ){
+	echo $argc;
+	echo "Usage:php Bundleup.php <full path of .ini> <name/version>" . PHP_EOL;
+}
+else{
+	$file = fopen("$argv[1]", "r") or die("Unable to open file!");
+	$archive = new PharData($argv[2].'.tar');
+	//$archive->addFile('text.txt');
+	$parse = parse_ini_file("$argv[1]",true);
+	mkdir('update',0777,true);
+	foreach($parse as $arr){
+		foreach($arr as $source){
+			$key = array_search($source, $arr);
+			copy($source, __DIR__.'/update/'.$key);
+			//echo 'Key: ' .$key . PHP_EOL;
+			//echo $val . PHP_EOL;
+
+		}
+	}
+	exec("cp $argv[1] " .__DIR__."/update/bundle.ini");
+	$archive->buildFromDirectory(dirname(__FILE__) . '/update');
+	//$archive->compress(Phar::GZ);
+	exec('rm -r update');
+	echo "Done!" .PHP_EOL;
+}
 ?>
