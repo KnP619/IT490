ced131bee4c9797e74bfc8b8af6ebbdb46ac9497
KnP619 <knp33@njit.edu>
2016-11-19 16:48:25 -0500

    for installing bundle

--- install.php
+++ install.php
@@ -1,31 +1,67 @@
 <?php
-if($argc != 2){
-	die("Usage: php install.php <path/to/tar.tar>".PHP_EOL);
-	}
-else{
-	$arch = new PharData("$argv[1]");
-	mkdir('update',0777,true);
-	$arch->extractTo(__DIR__.'/update/',null,true);
-	$file = fopen(__DIR__.'/update/bundle.ini', "r") or die("Unable to open file!");
-	$parse = parse_ini_file(__DIR__."/update/bundle.ini", true);
-	$check = 0;
-	foreach($parse as $arr){
-		foreach($arr as $dest){
-			if(!file_exists($dest) && $check == 0){
-				mkdir(dirname($dest),0777,true);
-				$check++;
-			}
-			$key = array_search($dest, $arr);
-			if(copy(__DIR__.'/update/'.$key, $dest)){
-				echo "copied to $dest" . PHP_EOL;
-				unlink(__DIR__.'/update/'.$key);
-			}
-			else{
-				die("Copy failed".PHP_EOL);
-			}
-		}
-	}
-	unlink(__DIR__.'/update/bundle.ini');
-	exec("rmdir update");
+require_once __DIR__ . '/vendor/autoload.php';
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+class FibonacciRpcClient {
+    private $connection;
+    private $channel;
+    private $callback_queue;
+    private $response;
+    private $corr_id;
+
+    public function __construct() {
+        $this->connection = new AMQPStreamConnection(
+            'localhost', 5672, 'admin', 'knp33');
+        $this->channel = $this->connection->channel();
+        list($this->callback_queue, ,) = $this->channel->queue_declare(
+            "", false, false, true, false);
+        $this->channel->basic_consume(
+            $this->callback_queue, '', false, false, false, false,
+            array($this, 'on_response'));
+    }
+    public function on_response($rep) {
+        if($rep->get('correlation_id') == $this->corr_id) {
+            $this->response = $rep->body;
+        }
+    }
+
+    public function call($n) {
+        $this->response = null;
+        $this->corr_id = uniqid();
+
+        $msg = new AMQPMessage(
+            (string) $n,
+            array('correlation_id' => $this->corr_id,
+                  'reply_to' => $this->callback_queue)
+            );
+        $this->channel->basic_publish($msg, '', 'bundle_install');
+        while(!$this->response) {
+            $this->channel->wait();
+        }
+        return intval($this->response);
+    }
+};
+
+$Deploy_Req=array();
+$Deploy_Req[0]='installer';
+$Deploy_Req[1]=$argv[1]; //Name of tar.gz //Name of task
+//$Deploy_Req[2] = $argv[2]; //Name of task
+if($argv[1] == NULL OR $argc != 2){
+	//echo "Usage: php install.php </path/to/my.tar.gz> <taskName>" . PHP_EOL;
+	echo "Usage: php install.php <taskName>" . PHP_EOL;
+	die();
 }
+$Deploy_Req_Final=json_encode($Deploy_Req);
+$fibonacci_rpc = new FibonacciRpcClient();
+$response = $fibonacci_rpc->call($Deploy_Req_Final);
+if ($response ==1){
+echo " [.] Bundle installed  successfully...\n";
+}
+else{
+	exit("Failed to install sucessfully");}
 ?>
+
+
+
+
